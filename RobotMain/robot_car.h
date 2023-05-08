0becdbcce70cd652243557ae769cdf3e126cec48
brandon-samario <115336934+brandon-samario@users.noreply.github.com>
2023-05-07 23:45:13 -0400

    Update robot_car.h

--- RobotMain/robot_car.h
+++ RobotMain/robot_car.h
@@ -6,17 +6,14 @@
 #include <Arduino.h>
 
 class RobotCar{
-  
   private:
     int _pinA1;
     int _pinA2;
     int _pinB1;
     int _pinB2;
     int _standby;
-  
   public:
     RobotCar(int _pinA1,int _pinA2,int _pinB1, int _pinB2, int _standby);
-
     void init();
     void forward();
     void backward();
@@ -24,7 +21,6 @@ class RobotCar{
     void right();
     void stop();
     
-
 };
 
 #endif
