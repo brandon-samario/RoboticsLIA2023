ca10aa3da627b48513568d5beebbb31def3b2643
brandon-samario <115336934+brandon-samario@users.noreply.github.com>
2023-05-07 22:26:06 -0400

    Add files via upload

---
+++ RobotMain/Ultrasonic.h
@@ -0,0 +1,15 @@
+
+
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+
+class Ultrasonic {
+  private:
+    int trigPin, echoPin;
+
+  public:
+    Ultrasonic(int trig, int echo);
+    int getDistance();
+};
+
+#endif
