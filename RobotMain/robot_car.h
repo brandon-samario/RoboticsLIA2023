408581396c531e4a726515b38e3e9469b93e8410
brandon-samario <115336934+brandon-samario@users.noreply.github.com>
2023-05-21 17:10:25 -0400

    robot_car.h

--- RobotMain/robot_car.h
+++ RobotMain/robot_car.h
@@ -1,25 +1,23 @@
-
-
 #ifndef ROBOT_CAR_H
 #define ROBOT_CAR_H
 
-#include <Arduino.h>
+#include <Arduino.h> // Include the Arduino library for using Arduino functions and types.
 
 class RobotCar{
   private:
-    int _pinA1;
-    int _pinA2;
-    int _pinB1;
-    int _pinB2;
-    int _standby;
+    int _pinA1; // Private variable to store the pin number for motor A1.
+    int _pinA2; // Private variable to store the pin number for motor A2.
+    int _pinB1; // Private variable to store the pin number for motor B1.
+    int _pinB2; // Private variable to store the pin number for motor B2.
+    int _standby; // Private variable to store the pin number for standby mode.
   public:
-    RobotCar(int _pinA1,int _pinA2,int _pinB1, int _pinB2, int _standby);
-    void init();
-    void forward();
-    void backward();
-    void left();
-    void right();
-    void stop();   
+    RobotCar(int _pinA1,int _pinA2,int _pinB1, int _pinB2, int _standby); // Constructor for the RobotCar class, which initializes the pin numbers.
+    void init(); // Public method to initialize the robot car.
+    void forward(); // Public method to move the robot car forward.
+    void backward(); // Public method to move the robot car backward.
+    void left(); // Public method to turn the robot car left.
+    void right(); // Public method to turn the robot car right.
+    void stop(); // Public method to stop the robot car.   
 };
 
 #endif
