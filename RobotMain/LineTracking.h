8f4c60f5c018d9851dcc7122ebff70f4b69788f5
brandon-samario <115336934+brandon-samario@users.noreply.github.com>
2023-05-21 16:58:22 -0400

    LineTracking.h

--- RobotMain/LineTracking.h
+++ RobotMain/LineTracking.h
@@ -1,22 +1,20 @@
-
-
 #ifndef LINETRACKING_H
 #define LINETRACKING_H
 
 class LineTracking {
   private:
-    int LT_R;
-    int LT_M;
-    int LT_L;
+    int LT_R; // Right line tracking sensor pin
+    int LT_M; // Middle line tracking sensor pin
+    int LT_L; // Left line tracking sensor pin
   public:
-    LineTracking();
-    void setup();
-    void move();
-    void forward();
-    void back();
-    void left();
-    void right();
-    void stop();
+    LineTracking(); // Constructor for the LineTracking class
+    void setup(); // Method to set up the line tracking system
+    void move(); // Method to initiate line tracking movement
+    void forward(); // Method to move forward
+    void back(); // Method to move backward
+    void left(); // Method to turn left
+    void right(); // Method to turn right
+    void stop(); // Method to stop movement
 };
 
 #endif
