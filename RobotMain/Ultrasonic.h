0e6711df919b1a268e6a461ddf5f8a5825c92421
brandon-samario <115336934+brandon-samario@users.noreply.github.com>
2023-05-21 17:05:57 -0400

    Ultrasonic.h

--- RobotMain/Ultrasonic.h
+++ RobotMain/Ultrasonic.h
@@ -1,14 +1,12 @@
-
-
-#ifndef ULTRASONIC_H
+#ifndef ULTRASONIC_H  // Header guard to prevent multiple inclusions of the header file
 #define ULTRASONIC_H
 
 class Ultrasonic {
   private:
-    int trigPin, echoPin;
+    int trigPin, echoPin;  // Private member variables to store the trigger and echo pins
   public:
-    Ultrasonic(int trig, int echo);
-    int getDistance();
+    Ultrasonic(int trig, int echo);  // Constructor to initialize the Ultrasonic object with trigger and echo pin numbers
+    int getDistance();  // Member function to retrieve the distance measured by the ultrasonic sensor
 };
 
-#endif
+#endif  // End of header guard
